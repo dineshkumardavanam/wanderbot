2f1654c5e0220c7bc0f790da66e66ec9a80c4a8e
Dinesh Kumar <96611161+dineshkumardavanam@users.noreply.github.com>
2024-10-29 16:23:04 -0400

    Small changes in UI

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Welcome to WanderBot</h1>
                 <Chatbot />
             </header>
         </div>
