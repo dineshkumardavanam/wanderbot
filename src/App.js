189ba36c0b1b9de9f553a9cb8bf3e12f6046d92c
Dinesh Kumar <dineshkumar@Mac.lan>
2024-10-29 16:36:08 -0400

    check and changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-<<<<<<< HEAD
                 <h1>    </h1>
-=======
-            <h1>    </h1>
->>>>>>> e6c73b8083723272a015842dbdbe7b4cff09fc87
                 <Chatbot />
             </header>
         </div>
